55d4ec695bc1b516a777783c9ca736f80e3e42ad
shepherd-001 <shepherdlegend001@gmail.com>
2024-12-01 11:21:51 +0100

    commit

--- cypress/e2e/jumia_spec.cy.js
+++ cypress/e2e/jumia_spec.cy.js
@@ -1,4 +1,4 @@
-// import "./01_jumia/01_visit_jumia.cy.js";
+import "./01_jumia/01_visit_jumia.cy.js";
 import "./01_jumia/02_searchProduct.cy.js";
-// import "./01_jumia/03_addProductToCart.cy.js";
-// import "./01_jumia/04_checkout.cy.js";
+import "./01_jumia/03_addProductToCart.cy.js";
+import "./01_jumia/04_checkout.cy.js";
